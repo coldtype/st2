3de28808a4bb0b1dad6658d1626221b36c495b71
Rob Stenson <rob.stenson@gmail.com>
2022-07-25 09:33:36 -0700

    metadata.py file and release.sh shortcut

---
+++ Coldtype/metadata.py
@@ -0,0 +1,11 @@
+bl_info = {
+    "name": "Coldtype",
+    "author": "Rob Stenson",
+    "version": (0, 2),
+    "blender": (3, 0, 0),
+    "location": "View3D > Toolshelf",
+    "description": "Well-shaped 3D typography",
+    "warning": "",
+    "wiki_url": "",
+    "category": "Coldtype",
+}
