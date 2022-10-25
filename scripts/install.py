6672faad31c70b1098882af9e604cf05ed235410
Rob Stenson <rob.stenson@gmail.com>
2022-10-25 09:03:29 -0700

    tweak for py2 friendliness

--- scripts/install.py
+++ scripts/install.py
@@ -4,6 +4,6 @@ sys.path.insert(0, ".")
 from scripts.blender import *
 
 if on_mac():
-    if addon.exists(): addon.unlink()
+    if os.path.exists(addon): os.unlink(addon)
     subprocess.run(["ln", "-s", addon_source, addon])
     sys.stdout.write(str(blender_executable))
