9d41d99945d2fe59811b355bbb901160ffd682d2
Rob Stenson <rob@goodhertz.com>
2023-01-15 09:58:56 -0800

    windows blender ftm

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 export BLENDER_PATH="~/Desktop/Blenders/Blender3.3.app"
+export BLENDER_PATH="C:\Program Files\Blender Foundation\Blender 3.4\blender.exe"
 alias bpy=$(b3denv bpy)
 alias blender=$(b3denv blender)
 b3denv install ST2
