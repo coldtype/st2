5d05ebbbf9f2feadfd977a7533ebacbce89ae803
Rob Stenson <rob@goodhertz.com>
2023-01-15 10:55:26 -0800

    3.3 for windows, tho yeah maybe this file just shouldn't be in git at all

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 export BLENDER_PATH="~/Desktop/Blenders/Blender3.4.app"
-export BLENDER_PATH="C:\Program Files\Blender Foundation\Blender 3.4\blender.exe"
+export BLENDER_PATH="C:\Program Files\Blender Foundation\Blender 3.3\blender.exe"
 alias bpy=$(b3denv bpy)
 alias blender=$(b3denv blender)
 b3denv install ST2
