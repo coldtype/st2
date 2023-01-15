3e830da8e4e54bc18331c9aab346ac104d904181
Rob Stenson <rob@goodhertz.com>
2023-01-15 10:56:09 -0800

    take setup.sh out of git

--- setup.sh
+++
@@ -1,5 +0,0 @@
-export BLENDER_PATH="~/Desktop/Blenders/Blender3.4.app"
-export BLENDER_PATH="C:\Program Files\Blender Foundation\Blender 3.3\blender.exe"
-alias bpy=$(b3denv bpy)
-alias blender=$(b3denv blender)
-b3denv install ST2
