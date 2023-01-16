785d53fbd2690f847cbf5aa98b1ce6fb36d7dcc9
Rob Stenson <rob@goodhertz.com>
2023-01-16 11:55:12 -0800

    delete local setup.sh

--- setup.sh
+++
@@ -1,5 +0,0 @@
-export BLENDER_PATH="~/Desktop/Blenders/Blender3.3.app"
-export BLENDER_PATH="C:\Program Files\Blender Foundation\Blender 3.4\blender.exe"
-alias bpy=$(b3denv bpy)
-alias blender=$(b3denv blender)
-b3denv install ST2
