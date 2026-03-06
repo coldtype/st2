dbecd516c460868472e3d31da3114ca28ee05643
Rob Stenson <rob@goodhertz.com>
2026-03-06 10:10:04 -0800

    slightly more complex example script to demonstrate using it on multiple objects

--- scripts/example.py
+++ scripts/example.py
@@ -6,8 +6,10 @@ def modify(st2:dict, kwargs:dict, p:P, funcs):
     #font = Font.Find(st2.font_path)
     #axis1_tag = list(font.variations().values())[0].get("axisTag")
 
-    return (Glyphwise((st2.text+"\n")*3, lambda x:
-        Style(st2.font_path, 2, **{"wdth":ez(funcs.fe(-x.i*11), "seio", 1)}))
+    oidx = kwargs.get("idx", 0)
+
+    return (Glyphwise((st2.text+"\n")*1, lambda x:
+        Style(st2.font_path, 2, **{"wdth":ez(funcs.fe(-x.i*(11-(oidx*5))), "seio", 1)}))
         .lead(0.1)
         .xalign()
         .pen()
